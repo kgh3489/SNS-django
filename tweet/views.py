75f2ef2c492f1877aa799faa3f75371b9011bf89
kgh <kgh3489@naver.com>
2021-09-04 03:55:28 +0900

    twwet 앱 추가

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def home(request):
+    user = request.user.is_authenticated #유저가 로그인(인증)이 되어있는지 확인
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('/sign-in')
+
+def tweet(request):
+    if request.method == 'GET':
+        return render(request, 'tweet/home.html')
