abe7c22d08dfc5739f9fd8d9f07637f12bded042
kgh <kgh3489@naver.com>
2021-09-04 03:56:41 +0900

    로그인 시 기본페이지로 경로 설정

--- user/models.py
+++ user/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import AbstractUser #Django의 기본 유저 모
 
 # Create your models here.
 class UserModel(AbstractUser):
+
     class Meta: #db 정보를 메타에 적어줌
         db_table = "my_user" #db table 이름을 지정
 
     # username = models.CharField(max_length=20, null=False)
     # password = models.CharField(max_length=256, null=False)
-    bio = models.CharField(max_length=256, default='')
+    bio = models.CharField(max_length=256, blank=True)
     # created_at = models.DateTimeField(auto_now_add=True)
     # updated_at = models.DateTimeField(auto_now=True)
 
--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model #사용자가 있는지 검사하는 함수
+from django.contrib import auth
 
 # 해당 html을 띄워주는 함수
 def sign_up_view(request):
@@ -15,30 +17,32 @@ def sign_up_view(request):
         if password != password2:
             return render(request, 'user/signup.html') #페이지를 다시 띄움
         else: #password가 제대로 입력 됐다면
-            exist_user = UserModel.objects.filter(username=username)
+            exist_user = get_user_model().objects.filter(username=username)
+            # exist_user = UserModel.objects.filter(username=username)
             if exist_user:
                 return render(request, 'user/signup.html')  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
             else: #데이터베이스에 저장
-                new_user = UserModel()
-                new_user.username = username
-                new_user.password = password
-                new_user.bio = bio
-                new_user.save()
-                return redirect('/sign-in')
-
-        return redirect('/sign-in') #회원가입 완료 시 로그인 페이지 실행
-
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                # new_user = UserModel()
+                # new_user.username = username
+                # new_user.password = password
+                # new_user.bio = bio
+                # new_user.save()
+                return redirect('/sign-in') #회원가입 완료 시 로그인 페이지 실행
 
 def sign_in_view(request):
     if request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         # post 요청에 담겨진 데이터를 받아옴
-        me = UserModel.objects.get(username=username) #UserModel db에서 username값이 받아온 데이터와 같은지 확인
-        if me.password == password: #me의 password값이 같은지 확인
-            request.session['user'] = me.username
-            return HttpResponse(me.username)
+        me = auth.authenticate(request, username=username, password=password) #암호화된 비밀번호와 실제 비밀번호가 맞는지 검사
+        #me = UserModel.objects.get(username=username) #UserModel db에서 username값이 받아온 데이터와 같은지 확인
+        #if me.password == password: #me의 password값이 같은지 확인
+        if me is not None:
+            auth.login(request, me)
+            #request.session['user'] = me.username
+            return redirect('/')
         else:
-            return  redirect('/sign-in')
+            return redirect('/sign-in')
     elif request.method == 'GET':
         return render(request, 'user/signin.html')
