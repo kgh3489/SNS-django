34601ce931ca917d1bf73f233d97c56db48a924b
kgh <kgh3489@naver.com>
2021-09-10 00:13:53 +0900

    게시글 읽기/삭제 기능 추가

--- tweet/views.py
+++ tweet/views.py
@@ -1,25 +1,34 @@
 from django.shortcuts import render, redirect
 from .models import TweetModel
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    user = request.user.is_authenticated #유저가 로그인(인증)이 되어있는지 확인
+    user = request.user.is_authenticated  # 유저가 로그인(인증)이 되어있는지 확인
     if user:
         return redirect('/tweet')
     else:
         return redirect('/sign-in')
 
+
 def tweet(request):
     if request.method == 'GET':
-        user = request.user.is_authenticated
+        user = request.user.is_authenticated  # 사용자가 로그인 되어있다면
         if user:
-            return render(request, 'tweet/home.html')
+            all_tweet = TweetModel.objects.all().order_by('-created_at')  # 모든 모델을 불러와 최신순으로 정렬
+            return render(request, 'tweet/home.html', {'tweet':all_tweet})
         else:
             return redirect('/sign-in')
 
     elif request.method == 'POST':
-        user = request.user #request.user:지금 로그인 되어있는 사용자의 정보를 불러움
+        user = request.user  # request.user:지금 로그인 되어있는 사용자의 정보를 불러움
         my_tweet = TweetModel()
         my_tweet.author = user
-        my_tweet.content = request.POST.get('my-content','')
+        my_tweet.content = request.POST.get('my-content', '')
         my_tweet.save()
+        return redirect('/tweet')
+
+@login_required
+def delete_tweet(request, id):
+        my_tweet = TweetModel.objects.get(id=id)
+        my_tweet.delete()
         return redirect('/tweet')
