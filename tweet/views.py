829c45b963b627a15046a05eb54e1dfd360a9a72
kgh <kgh3489@naver.com>
2021-10-10 22:42:17 +0900

    comment 에러 메시지 추가

--- tweet/views.py
+++ tweet/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from .models import TweetModel, TweetComment
 from django.contrib.auth.decorators import login_required
 
+
 def home(request):
     user = request.user.is_authenticated  # 유저가 로그인(인증)이 되어있는지 확인
     if user:
         return redirect('/tweet')
     else:
         return redirect('/sign-in')
+
 
 def tweet(request):
     if request.method == 'GET':
@@ -20,11 +22,19 @@ def tweet(request):
 
     elif request.method == 'POST':
         user = request.user  # request.user:지금 로그인 되어있는 사용자의 정보를 불러움
-        my_tweet = TweetModel()
-        my_tweet.author = user
-        my_tweet.content = request.POST.get('my-content', '')
-        my_tweet.save()
-        return redirect('/tweet')
+        content = request.POST.get('my-content', '')
+        if content == '': #글쓰기 부분이 비어있다면
+            all_tweet = TweetModel.objects.all().order_by('-created_at')
+            return render(request, 'tweet/home.html', {'error': '글은 공백일 수 없습니다!', 'tweet': all_tweet})
+        else:
+            # my_tweet = TweetModel()
+            # my_tweet.author = user
+            # my_tweet.content = request.POST.get('my-content', '')
+            # my_tweet.save()
+            my_tweet = TweetModel.objects.create(author=user, content=content)
+            my_tweet.save()
+            return redirect('/tweet')
+
 
 @login_required
 def delete_tweet(request, id):
@@ -32,16 +42,18 @@ def delete_tweet(request, id):
     my_tweet.delete()
     return redirect('/tweet')
 
+
 @login_required
 def detail_tweet(request, id):
     my_tweet = TweetModel.objects.get(id=id)
-    tweet_comment = TweetComment.objects.filter(tweet_id=id).order_by('-created_at') #해당 트윗아이디와 같은지 확인
-    return render(request,'tweet/tweet_detail.html',{'tweet':my_tweet,'comment':tweet_comment})
+    tweet_comment = TweetComment.objects.filter(tweet_id=id).order_by('-created_at')  # 해당 트윗아이디와 같은지 확인
+    return render(request, 'tweet/tweet_detail.html', {'tweet': my_tweet, 'comment': tweet_comment})
+
 
 @login_required
 def write_comment(request, id):
     if request.method == 'POST':
-        comment = request.POST.get("comment","")
+        comment = request.POST.get("comment", "")
         current_tweet = TweetModel.objects.get(id=id)
 
         TC = TweetComment()
@@ -50,13 +62,12 @@ def write_comment(request, id):
         TC.tweet = current_tweet
         TC.save()
 
-        return redirect('/tweet/'+str(id))
+        return redirect('/tweet/' + str(id))
+
 
 @login_required
 def delete_comment(request, id):
     comment = TweetComment.objects.get(id=id)
     current_tweet = comment.tweet.id
     comment.delete()
-    return redirect('/tweet/'+str(current_tweet))
-
-
+    return redirect('/tweet/' + str(current_tweet))
